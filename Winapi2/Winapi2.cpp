3bb4fb230d2fd4f0fd3b10b197b87d63b980cdb7
Leejunhyeok <leejh3907@naver.com>
2022-10-18 01:30:12 +0900

    insert Test

--- Winapi2/Winapi2.cpp
+++ Winapi2/Winapi2.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // TODO: 여기에 코드를 입력합니다.
     // Git Test
-    // 
+    // 바뀌나?
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_WINAPI2, szWindowClass, MAX_LOADSTRING);
